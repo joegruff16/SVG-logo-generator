356645e1ea6bd4a84f0103db65b5160f66f381f0
Joseph Ruff <joegruff16@gmail.com>
2024-07-12 18:49:51 -0400

    Added a script to start the application. Added boiler plate code for the inquirer prompts and answers.

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 // Include packages to use in project
-const inquirer = require('inquire');
+const inquirer = require("inquire");
 
 // Set a list of prompts to to help the user generate text, shapes, and color of the shapes
-
+// Boiler plate code that can be adjusted
+inquirer
+  .prompt([
+    // Pass prompts or questions here
+  ])
+  .then((answers) => {
+    // any feedback validate answers
+  })
+  .catch((error) => {
+    // Catch used to catch errors in code
+    if (error) {
+      // Prompt couldn't rendered
+    } else {
+      // Other errors
+    }
+  });
 // Use the write file function and path to generate a the shapes in a specified location the file should be named logo.svg
 
 // fs.writeFile(fileName, data)
